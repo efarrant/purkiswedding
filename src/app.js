fcf1807c139715cd43db8303f43c7fbb2c2d70ab
efarrant <83728803+efarrant@users.noreply.github.com>
2021-08-23 02:04:14 +0100

    password protect wedding website

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 function Password() {
     let password = prompt("Welcome to Emma and Sam's wedding website! Please enter the password:");
-    if (password.value=="purkiswedding2021") {
+    if (password=="purkiswedding2021") {
     alert("Thank you!")
     }
     else {
-        break
+        alert("Sorry, this is not correct");
+        window.stop();
+
     }
 }
 
