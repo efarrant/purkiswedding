d255b3f9883e44667af903a769e1dd006839c9f5
efarrant <83728803+efarrant@users.noreply.github.com>
2021-08-23 01:33:46 +0100

    structure

---
+++ src/app.js
@@ -0,0 +1,9 @@
+function password() {
+    let password = prompt("Welcome to Emma and Sam's wedding website! Please enter the password:");
+    if (password.value=="purkiswedding2021") {
+    alert("Thank you!")
+    }
+    else {
+        break
+    }
+}
