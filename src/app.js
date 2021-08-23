4127815f936cc6e7aecc9934f802b23f5b03c3f7
efarrant <83728803+efarrant@users.noreply.github.com>
2021-08-23 01:39:55 +0100

    password

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-function password() {
+function Password() {
     let password = prompt("Welcome to Emma and Sam's wedding website! Please enter the password:");
     if (password.value=="purkiswedding2021") {
     alert("Thank you!")
@@ -7,3 +7,5 @@ function password() {
         break
     }
 }
+
+document.addEventListener("DOMContentLoaded", Password());
